90e74896df8a33fb6128d65e5b0d1db9101b71fb
vankotraining <74247018+vankotraining@users.noreply.github.com>
2026-07-06 11:57:56 +0200

    Wire client deletion into knee dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ButtonGuards from "./components/ButtonGuards";
+import ClientDeletion from "./components/ClientDeletion";
 import KneeDashboard from "./components/KneeDashboard";
 
 export default function Home() {
   return (
     <>
       <KneeDashboard />
+      <ClientDeletion />
       <ButtonGuards />
     </>
   );
