382ea7382e7da01dfa4111afcb8bdd1f7180a70a
vankotraining <74247018+vankotraining@users.noreply.github.com>
2026-08-03 22:53:31 +0200

    Keep client actions on main Knee page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,5 @@
-import Link from "next/link";
 import KneeApp from "./components/KneeApp";
 
 export default function Home() {
-  return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "12px 16px 0",
-        }}
-      >
-        <Link
-          href="/tindeq"
-          style={{
-            border: "1px solid rgba(105, 216, 173, 0.45)",
-            borderRadius: 999,
-            color: "inherit",
-            fontWeight: 700,
-            padding: "9px 14px",
-            textDecoration: "none",
-          }}
-        >
-          Tindeq Repeaters
-        </Link>
-      </div>
-      <KneeApp />
-    </>
-  );
+  return <KneeApp />;
 }
